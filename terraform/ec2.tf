920e8713853e87fe06d38bc22422696f3f175abc
Vinodhini <Vinodhini0208@gmail.com>
2025-09-23 06:44:44 +0000

    Fix ec2.tf user_data: pass correct variables to templatefile

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,17 +1,15 @@
-locals {
-  ecr_image_uri = "${var.ecr_registry}/${var.ecr_repository}:${var.image_tag}"
-}
-
 resource "aws_instance" "strapi" {
-  ami                    = "ami-0c55b159cbfafe1f0"
-  instance_type          = "t2.micro"
-  key_name               = var.key_name
-  security_groups        = [aws_security_group.strapi_sg.name]
-  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
+  ami                  = "ami-0c55b159cbfafe1f0"
+  instance_type        = "t2.micro"
+  key_name             = var.key_name
+  security_groups      = [aws_security_group.strapi_sg.name]
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
 
   user_data = templatefile("${path.module}/userdata.tpl", {
-    ecr_image_uri = local.ecr_image_uri
-    region        = var.aws_region
+    REGISTRY   = var.ecr_registry
+    REPOSITORY = var.ecr_repository
+    TAG        = var.image_tag
+    AWS_REGION = var.aws_region
   })
 
   tags = {
