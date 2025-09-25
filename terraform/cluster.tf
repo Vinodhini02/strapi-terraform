46f2d77c43f4c2f6fc91a49a1daad09ae769b11b
Vinodhini <Vinodhini0208@gmail.com>
2025-09-25 09:39:26 +0000

    edited cluster.tf

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -7,6 +7,9 @@ data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_subnet_ids" "public" {
-  vpc_id = data.aws_vpc.default.id
+data "aws_subnets" "public" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
